dbc2de15114b1bff2e26a53993135ba5ebd7cd77
Remi-Debu <remi.debu@free.fr>
2023-02-02 10:42:13 +0100

    init projet mobile

---
+++ mobile/src/core/Menu.tsx
@@ -0,0 +1,68 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+} from '@ionic/react';
+
+import { useLocation } from 'react-router-dom';
+import { carOutline, carSharp, manOutline, manSharp, repeatOutline, repeatSharp } from 'ionicons/icons';
+import './Menu.css';
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+const appPages: AppPage[] = [
+  {
+    title: 'Locataires',
+    url: '/page/Locataires',
+    iosIcon: manOutline,
+    mdIcon: manSharp
+  },
+  {
+    title: 'Véhicules',
+    url: '/page/Vehicules',
+    iosIcon: carOutline,
+    mdIcon: carSharp
+  },
+  {
+    title: 'Locations',
+    url: '/page/Locations',
+    iosIcon: repeatOutline,
+    mdIcon: repeatSharp
+  }
+];
+
+const Menu: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+        <IonList id="inbox-list">
+          <IonListHeader>Localib</IonListHeader>
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default Menu;
